cee7f1b5d7a096e5abbef69d47236f912e2e4397
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-05-05 23:44:36 +0530

    docs: Add comments to main.go

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 func main() {
 
+	// Create a new instance of APIServer listening on port 3000.
 	server := newAPIServer(":3000")
+
+	// Run the server to start handling incoming HTTP requests.
 	server.Run()
 
 }
