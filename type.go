73f1f2478c56383ce691022c7b9c293414839586
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-05-05 23:44:59 +0530

    docs: Add comments to type.go

--- type.go
+++ type.go
@@ -2,6 +2,7 @@ package main
 
 import "math/rand"
 
+// Account represents a bank account.
 type Account struct {
 	ID        int    `json:"id"`
 	FirstName string `json:"firstName"`
@@ -10,6 +11,7 @@ type Account struct {
 	Balance   int64  `json:"balance"`
 }
 
+// NewAccount creates a new Account instance with random ID and account number.
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
 		ID:        rand.Intn(10000),
