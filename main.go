a9f78e1e6ad383a5c3ee817513072a9d1badc67b
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-05-07 19:17:38 +0530

    feat: Integrate database initialization into main function

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
 
+	// Initialize a new instance of PostgresStore.
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialize the database by creating necessary tables.
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
 	// Create a new instance of APIServer listening on port 3000.
-	server := newAPIServer(":3000")
+	// and pass the initialized store as a parameter.
+	server := newAPIServer(":3000", store)
 
 	// Run the server to start handling incoming HTTP requests.
 	server.Run()
