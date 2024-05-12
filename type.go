e1f9b9a00291464bb277715e7c76267a53d2a0c3
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-05-12 11:05:54 +0530

    feat: Add password handling to Account struct and related methods

--- type.go
+++ type.go
@@ -3,35 +3,63 @@ package main
 import (
 	"math/rand"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+// LoginResponse represents the response sent back to the client after a successful login.
+type LoginResponse struct {
+	Number int64  `json:"number"` // Unique identifier for the account
+	Token  string `json:"token"` // Authentication token for subsequent requests
+}
+
+type LoginRequest struct {
+	Number   int64  `json:"number"`
+	Password string `json:"password"`
+}
 
 type TransferRequest struct {
 	ToAccount int `json:"toAccount"`
-	Amount int `json:"amount"`
+	Amount    int `json:"amount"`
 }
 
 // CreateAccountRequest represents a request to create a new account.
 type CreateAccountRequest struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
+	Password  string `json:"password"`
 }
 
 // Account represents a bank account.
 type Account struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	Number    int64     `json:"number"`
-	Balance   int64     `json:"balance"`
-	CreatedAt time.Time `json:"createdAt"`
+	ID                int       `json:"id"`
+	FirstName         string    `json:"firstName"`
+	LastName          string    `json:"lastName"`
+	Number            int64     `json:"number"`
+	EncryptedPassword string    `json:"-"`
+	Balance           int64     `json:"balance"`
+	CreatedAt         time.Time `json:"createdAt"`
+}
+
+// ValidPassword checks if the provided password matches the account's encrypted password.
+func (a *Account) ValidPassword(password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(a.EncryptedPassword), []byte(password)) == nil
 }
 
 // NewAccount creates a new Account instance with random ID and account number.
-func NewAccount(firstName, lastName string) *Account {
-	return &Account{
-		FirstName: firstName,
-		LastName:  lastName,
-		Number:    int64(rand.Intn(1000000)),
-		CreatedAt: time.Now().UTC(),
+func NewAccount(firstName, lastName, password string) (*Account, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+
 	}
+
+	 // Create and return the new Account instance
+	return &Account{
+		FirstName:         firstName,
+		LastName:          lastName,
+		EncryptedPassword: string(encpw),
+		Number:            int64(rand.Intn(1000000)),
+		CreatedAt:         time.Now().UTC(),
+	}, nil
 }
